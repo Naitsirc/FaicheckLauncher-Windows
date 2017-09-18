1f63d4ce3879a6980b72cce52e66e5e6b34a5885
verditerdev <31409706+verditerdev@users.noreply.github.com>
2017-09-18 15:20:56 +0200

    Lanzador sencillo de Faicheck para sistemas operativos Windows

---
+++ Faicheck/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Faicheck
+{
+    partial class FaicheckLauncher
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FaicheckLauncher));
+            this.errorJava = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Configuracion = new System.Windows.Forms.Panel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorJava.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.Configuracion.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // errorJava
+            // 
+            this.errorJava.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorJava.BackColor = System.Drawing.Color.Snow;
+            this.errorJava.Controls.Add(this.textBox2);
+            this.errorJava.Controls.Add(this.panel2);
+            this.errorJava.Controls.Add(this.label1);
+            this.errorJava.Location = new System.Drawing.Point(1, 1);
+            this.errorJava.Name = "errorJava";
+            this.errorJava.Size = new System.Drawing.Size(634, 431);
+            this.errorJava.TabIndex = 0;
+            this.errorJava.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.textBox2.Location = new System.Drawing.Point(87, 381);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(446, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Lanzador de Faicheck, la aplicación NO OFICIAL de Faitic.";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(87, 126);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(446, 199);
+            this.panel2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F);
+            this.textBox1.Location = new System.Drawing.Point(25, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(395, 76);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "No se ha encontrado Java instalado en el equipo. Será necesario instalar Java ant" +
+    "es de proseguir con la ejecución para el correcto funcionamiento del programa.";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(334, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reintentar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.YellowGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(197, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Descargar Java";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(94, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Existe un problema con la instalación";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Configuracion
+            // 
+            this.Configuracion.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Configuracion.BackColor = System.Drawing.Color.Snow;
+            this.Configuracion.Controls.Add(this.progressBar);
+            this.Configuracion.Controls.Add(this.textBox3);
+            this.Configuracion.Controls.Add(this.panel3);
+            this.Configuracion.Controls.Add(this.label2);
+            this.Configuracion.Location = new System.Drawing.Point(2, 1);
+            this.Configuracion.Name = "Configuracion";
+            this.Configuracion.Size = new System.Drawing.Size(634, 431);
+            this.Configuracion.TabIndex = 6;
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.progressBar.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.progressBar.Location = new System.Drawing.Point(86, 331);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(447, 19);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.textBox3.Location = new System.Drawing.Point(87, 381);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(446, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Lanzador de Faicheck, la aplicación NO OFICIAL de Faitic.";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.textBox4);
+            this.panel3.Location = new System.Drawing.Point(87, 108);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(446, 199);
+            this.panel3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Enabled = false;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F);
+            this.textBox4.Location = new System.Drawing.Point(25, 27);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(395, 146);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Text = "Bienvenido al lanzador de la aplicación. Antes de continuar, selecciona el tipo d" +
+    "e instalación que deseas. Nosotros nos encargaremos del resto.";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(117, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Selecciona el tipo de instalación";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FaicheckLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 431);
+            this.Controls.Add(this.Configuracion);
+            this.Controls.Add(this.errorJava);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FaicheckLauncher";
+            this.Text = "Faicheck Launcher";
+            this.errorJava.ResumeLayout(false);
+            this.errorJava.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.Configuracion.ResumeLayout(false);
+            this.Configuracion.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel errorJava;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel Configuracion;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Faicheck/Form1.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Web;
+using System.IO;
+using System.Reflection;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Faicheck
+{
+    public partial class FaicheckLauncher : Form
+    {
+        static String VERSION = "v1.1.0";
+
+
+        public FaicheckLauncher()
+        {
+            InitializeComponent();
+            inicio();
+            
+        }
+
+        public static void CopyStream(Stream input, Stream output)
+        {
+            // Insert null checking here for production
+            byte[] buffer = new byte[8192];
+
+            int bytesRead;
+            while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, bytesRead);
+            }
+        }
+
+        void lanzar_deprecated()
+        {
+
+            try
+            {
+                Assembly _assembly;
+                Stream _imageStream;
+
+                if (System.IO.File.Exists(Environment.CurrentDirectory + @"\Faicheck.jar"))
+
+                {
+
+                    //delete
+
+                    System.IO.File.Delete(Environment.CurrentDirectory + @"\Faicheck.jar");
+                }
+
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("Faicheck.Faicheck.jar");
+
+                using (Stream input = _imageStream)
+                using (Stream output = File.Create(Environment.CurrentDirectory + @"\Faicheck.jar"))
+                {
+                    CopyStream(input, output);
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Error accessing resources!");
+            }
+
+
+
+            
+            
+
+
+            var processInfo = new ProcessStartInfo("java.exe", @"-jar Faicheck.jar")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            Process proc;
+
+            if ((proc = Process.Start(processInfo)) == null)
+            {
+                throw new InvalidOperationException("??");
+            }
+
+            proc.WaitForExit();
+            int exitCode = proc.ExitCode;
+            // proc.Close();
+
+            salir();
+
+        }
+
+        void lanzar()
+        {
+
+            lanzar(Environment.CurrentDirectory);// + @"\Faicheck");
+
+           /* if (tipo() == 2)
+            {
+                lanzar(Environment.CurrentDirectory);// + @"\.faitic_checker");
+            }
+            else
+            {
+                lanzar(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+@"\.faitic_checker");
+            }
+            */
+        }
+
+       
+
+        void lanzar(String path)
+        {
+            //MessageBox.Show("Lanzar: "+ "java.exe" + " " + @"-jar " + path + @"\Faicheck.jar");
+
+            if (!File.Exists(path + @"\Faicheck\Faicheck.jar"))
+            {
+                iniciar_jar(path);
+            }
+            else {
+
+                var processInfo = new ProcessStartInfo("java.exe", @"-jar " + path + @"\Faicheck\Faicheck.jar")
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+                Process proc;
+
+                if ((proc = Process.Start(processInfo)) == null)
+                {
+                    throw new InvalidOperationException("??");
+                }
+
+                //proc.WaitForExit();
+                //int exitCode = proc.ExitCode;
+                // proc.Close();
+
+                salir();
+            }
+
+        }
+
+        Boolean JAVA()
+        {
+            Boolean java = true;
+
+            try
+            {
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("java", "-version ");
+
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.RedirectStandardError = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                proc.StandardError.ReadToEnd();
+
+            }
+            catch (Exception objException)
+            {
+                java = false;
+            }
+
+            return java;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://java.com/en/download/manual.jsp");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int tipo()
+        {
+            int res = 0;
+
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.faitic_checker\login.conf";
+
+            if (System.IO.File.Exists(path)) {
+                res = 1;
+            } else {
+                String path2 = Environment.CurrentDirectory + @"\faitic_checker-settings\login.conf";
+
+                if (System.IO.File.Exists(path2))
+                {
+                    res = 2;
+                }
+                else {
+
+                }
+            }
+
+
+            return res;
+
+        }
+
+        Boolean configurado()
+        {
+            return tipo()!=0;
+        }
+
+        void inicio()
+        {
+            if (JAVA()) {
+
+                iniciar(Environment.CurrentDirectory);
+
+                errorJava.Visible = false;
+                Configuracion.Visible = false;
+
+                /*if (!configurado())
+                {
+                    Configuracion.Visible = true;
+                }
+                else {
+                    lanzar();
+                }*/
+            } else {
+                errorJava.Visible = true;
+                Configuracion.Visible = false;
+            }
+        }
+
+        void salir()
+        {
+            /*if (File.Exists(Environment.CurrentDirectory + @"\Faicheck.jar"))
+            {
+                File.Delete(Environment.CurrentDirectory + @"\Faicheck.jar");
+            }*/
+
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                // WinForms app
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                // Console app
+                System.Environment.Exit(1);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            iniciar(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            iniciar(Environment.CurrentDirectory);
+        }
+
+        void iniciar(String path)
+        {
+            String nombre = "";
+
+
+            /*if (path.Contains(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))) {
+                nombre = @"\.faitic_checker\";
+                path_selec = path + @"\.faitic_checker\";
+            }
+            else
+            {
+                nombre = @"\faitic_checker-settings";
+                path_selec = path;
+            }
+            */
+
+            nombre = @"\Faicheck";
+            path_selec = path;
+
+            bool exists = System.IO.Directory.Exists(path + nombre);
+
+            if (!exists)
+            {
+                System.IO.Directory.CreateDirectory(path + nombre);
+            }
+
+            bool exists2 = System.IO.Directory.Exists(path + nombre + @"\faitic_checker-settings");
+
+            if (!exists2)
+            {
+                System.IO.Directory.CreateDirectory(path + nombre + @"\faitic_checker-settings");
+            }
+
+            if (!File.Exists(path + nombre + @"\faitic_checker-settings\login.conf"))
+            {
+
+                System.IO.File.WriteAllLines(path + nombre + @"\faitic_checker-settings\login.conf", new string[] { "{\"Language\":\"es\" });//,\"Username\":\"\"}" });
+
+                //File.Create(path + @"\.faitic_checker\login.conf");
+                //TextWriter tw = new StreamWriter(path + @"\.faitic_checker\login.conf");
+                //tw.WriteLine("{\"Language\":\"es\",\"Username\":\"00000000\"}");
+                //tw.Close();
+            }
+
+            iniciar_jar(path);
+
+            lanzar();
+
+            
+        }
+
+        void iniciar_jar(String path) { 
+
+            try
+            {
+                Assembly _assembly;
+                Stream _imageStream;
+
+                if (!System.IO.File.Exists(path_selec + @"\Faicheck\Faicheck.jar"))
+
+                {
+
+                    
+
+                        _assembly = Assembly.GetExecutingAssembly();
+                        _imageStream = _assembly.GetManifestResourceStream("Faicheck.Faicheck.jar");
+
+                        using (Stream input = _imageStream)
+                        using (Stream output = File.Create(Environment.CurrentDirectory + @"\Faicheck\Faicheck.jar"))
+                        {
+                            CopyStream(input, output);
+                        }
+                    
+
+                }
+
+                
+            }
+            catch
+            {
+                // MessageBox.Show("Error accessing resources!");
+            }
+        }
+
+        String path_selec = "";
+
+        void CONTINUAR_INSTALACION()
+        {
+            try { 
+
+            string uno = Environment.CurrentDirectory + @"\Faicheck.jar";
+            String dos = path_selec + @"\Faicheck.jar";
+
+            if (!uno.Contains(dos))
+            {
+                File.Copy(uno, dos);
+                System.IO.File.Delete(Environment.CurrentDirectory + @"\Faicheck.jar");
+            }
+        }
+            catch
+            {
+                // MessageBox.Show("Error accessing resources!");
+            }
+
+            lanzar();
+        }
+       
+
+        void DESCARGAR(String url, String path)
+        {
+           
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFileAsync(new Uri(url), path + @"\Faicheck.jar");
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            CONTINUAR_INSTALACION();
+        }
+
+        String NUEVA_VERSION()
+        {
+            String result = null;
+
+            using (WebClient client = new WebClient()) 
+            {
+                //client.DownloadFile("https://davovoid.github.io/update-v1.json", @"C:\localfile.html");
+
+                string htmlCode = client.DownloadString("https://davovoid.github.io/update-v1.json");
+
+                if (!htmlCode.Contains("\"currentversion\":\"" + VERSION + "\""))
+                {
+                    result = Regex.Split(Regex.Split(htmlCode, "\"downloadurl\":\"")[1],"\"")[0].Replace("\\/", "/");
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
